225b480f918e21c540aa5035270106de0b570131
Govind <govind12771277@gmail.com>
2026-03-07 00:04:46 +0530

    deployment change vercel --> github pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: process.env.PAGES_BASE_PATH,
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
