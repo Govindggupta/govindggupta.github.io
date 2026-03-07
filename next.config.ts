62a1a8ad825bf45666e3fb3f6c7dacb5d11aac21
Govind <govind12771277@gmail.com>
2026-03-07 11:24:57 +0530

    next config change

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
+  trailingSlash: true,
   basePath: process.env.PAGES_BASE_PATH,
+  env: {
+    NEXT_PUBLIC_BASE_PATH: process.env.PAGES_BASE_PATH ?? "",
+  },
   images: {
     unoptimized: true,
     remotePatterns: [
