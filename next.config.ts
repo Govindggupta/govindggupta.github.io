3b7c4d4290392e19f8e78061fa697deb6458742e
Govind <govind12771277@gmail.com>
2026-03-07 12:41:11 +0530

    preview.yml change

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,12 @@
 import type { NextConfig } from "next";
 
+const isGitHubPages = process.env.DEPLOY_TARGET === 'github-pages';
+
 const nextConfig: NextConfig = {
   output: "export",
   trailingSlash: true,
   basePath: process.env.PAGES_BASE_PATH,
+  assetPrefix: isGitHubPages ? `/${process.env.BASE_PATH}/` : '',
   env: {
     NEXT_PUBLIC_BASE_PATH: process.env.PAGES_BASE_PATH ?? "",
   },
